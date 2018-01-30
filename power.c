388d5f08ca4d1b7f07e4983210bd9d157c3176be
AshiGupta1998 <35255600+AshiGupta1998@users.noreply.github.com>
2018-01-30 20:49:12 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int n,m,p;
+	printf("enter a number");
+	scanf("%d",&n);
+		printf("enter a number");
+	scanf("%d",&m);
+	p=pow(n,m);
+	printf("power of a num is %d", p);
+}
